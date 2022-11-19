34a773058fecf11523b65adc1c152fea1b14a1e2
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-19 23:12:54 +0200

    add app routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider , Navigate } from "react-router-dom";
 import Layout from '../Layout/Layout';
 import NotFound from '../NotFound/NotFound';
 import Login from '../Login/Login'
@@ -6,6 +6,11 @@ import Register from '../Register/Register'
 import Home from '../Home/Home';
 import All from '../All/All'
 import Games from '../Games/Games'
+import DisplayedGames from "../DisplayGames/DisplayedGames";
+import PlatForm from "../PlatForms/PlatForm";
+import Loading from '../Loading/Loading';
+import Sort from '../Sort/Sort';
+import Category from '../Categories/Category'
 
 function App() {
   const routes=createBrowserRouter([
@@ -14,13 +19,38 @@ function App() {
       {path:'login',element:<Login/>},
       {path:'register',element:<Register/>},
       {path:'games',element:<Games/>,children:[
-        {path:'all',element:<All/>}
+        {path:'all',element:<All/>},
+        {path:'Platforms',element:<PlatForm/>,children:[
+          {index:true,element:<Loading/>},
+          {path:'pc',element:<DisplayedGames/>},
+          {path:'browser',element:<DisplayedGames/>}
+        ]},
+        {path:'sort-by',element:<Sort/>,children:[
+          {path:'release-date',element:<DisplayedGames/>},
+          {path:'popularity',element:<DisplayedGames/>},
+          {path:'relevance',element:<DisplayedGames/>},
+          {path:'alphabetical',element:<DisplayedGames/>}
+        ]},
+        {path:'Categories',element:<Category/>,children:[
+          {index:true,element:<Loading/>},
+          {path:'racing',element:<DisplayedGames/>},
+          {path:'sports',element:<DisplayedGames/>},
+          {path:'social',element:<DisplayedGames/>},
+          {path:'shooter',element:<DisplayedGames/>},
+          {path:'open-world',element:<DisplayedGames/>},
+          {path:'zombie',element:<DisplayedGames/>},
+          {path:'fantasy',element:<DisplayedGames/>},
+          {path:'action-rpg',element:<DisplayedGames/>},
+          {path:'action',element:<DisplayedGames/>},
+          {path:'flight',element:<DisplayedGames/>},
+          {path:'battle-royale',element:<DisplayedGames/>},
+        ]
+        }
       ]}
     ]}
   ])
   return (
     <div className="App">
-        <h1>app</h1>
         <RouterProvider router={routes}></RouterProvider>
     </div>
   );
