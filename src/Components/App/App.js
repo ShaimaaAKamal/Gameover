98274b14af3ffc2529b704a4466c31c5f46d435d
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-19 22:26:08 +0200

    init inital routes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,8 +1,27 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from '../Layout/Layout';
+import NotFound from '../NotFound/NotFound';
+import Login from '../Login/Login'
+import Register from '../Register/Register'
+import Home from '../Home/Home';
+import All from '../All/All'
+import Games from '../Games/Games'
 
 function App() {
+  const routes=createBrowserRouter([
+    {path:'',element:<Layout/>,errorElement:<NotFound/>,children:[
+      {path:'home',element:<Home/>},
+      {path:'login',element:<Login/>},
+      {path:'register',element:<Register/>},
+      {path:'games',element:<Games/>,children:[
+        {path:'all',element:<All/>}
+      ]}
+    ]}
+  ])
   return (
     <div className="App">
         <h1>app</h1>
+        <RouterProvider router={routes}></RouterProvider>
     </div>
   );
 }
