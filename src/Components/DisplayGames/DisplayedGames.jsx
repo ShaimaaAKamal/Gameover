e16496951f92b441646ab12e0c21209da9c0299d
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-19 23:12:01 +0200

    init displaygames  component

---
+++ src/Components/DisplayGames/DisplayedGames.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class DisplayedGames extends Component {
+  render() {
+    return (
+      <div>DisplayedGames</div>
+    )
+  }
+}
+
+export default DisplayedGames
