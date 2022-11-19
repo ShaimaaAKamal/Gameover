3b9296749cc0d2b1511fedcbf32716aa5404d687
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-19 23:13:36 +0200

    add Outlet to Components so as to be able to use nested routing

--- src/Components/Categories/Category.jsx
+++ src/Components/Categories/Category.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export default function Category() {
   return (
-    <div>Category</div>
+    <Outlet/>
   )
 }
--- src/Components/Sort/Sort.jsx
+++ src/Components/Sort/Sort.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 export default function sort() {
   return (
-    <div>sort</div>
+    <Outlet/>
   )
 }
