874ff7a5eeadd75e94035b11701ae68e3ae9aca7
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-19 20:24:01 +0200

    install fortawesome and attach it to the app

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './Components/App/App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
