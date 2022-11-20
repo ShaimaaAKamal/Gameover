b452f689aa1289d4364abf0d68c8ef8a13f386e5
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-20 11:58:41 +0200

    init register component implementation

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import auth from '../../images/auth.jpg'
 
 export default function Register() {
   return (
-    <div>Register</div>
+    <div className="container py-5 my-5">
+        <div className="row">
+          <div className="col-lg-6 d-none d-lg-block">
+           <div>
+            <img src={auth} alt="siteImage" className='w-100'/>
+           </div>
+          </div>
+          <div className="col-lg-6">
+            <div>
+            </div>
+          </div>
+        </div>
+    </div>
   )
 }
