efe3eb37fef9818db520ddddf95b7816223d744d
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-20 11:15:05 +0200

    nav component html implementation

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,32 +1,71 @@
-import React from 'react'
+import React from 'react';
+import { Link ,NavLink} from 'react-router-dom';
+import logo from '../../images/logo.png'
 
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-light">
-      <div className="container-fluid">
-        <a className="navbar-brand" href="#">Navbar</a>
+      <div className="container">
+        <Link className="navbar-brand" to="home">
+          <img src={logo} alt="logoImage" className='me-2' style={{width:'50px'}} />
+          <span>Game Over</span>
+        </Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav me-auto ms-lg-4 mb-2 mb-lg-0">
                   <li className="nav-item">
-                    <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    <NavLink className="nav-link" aria-current="page" to="home">Home</NavLink>
                   </li>
                   <li className="nav-item">
-                    <a className="nav-link" href="#">Link</a>
+                    <NavLink className="nav-link" to="games/all">All</NavLink>
                   </li>
                   <li className="nav-item dropdown">
-                    <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                      Dropdown
-                    </a>
+                    <NavLink className="nav-link dropdown-toggle" to="games/Platforms" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Platforms
+                    </NavLink>
                     <ul className="dropdown-menu">
-                      <li><a className="dropdown-item" href="#">Action</a></li>
-                      <li><a className="dropdown-item" href="#">Another action</a></li>
-                      <li><a className="dropdown-item" href="#">Something else here</a></li>
+                      <li><NavLink className="dropdown-item" to="games/Platforms/pc">Pc</NavLink></li>
+                      <li><NavLink className="dropdown-item" to="games/Platforms/browser">Browser</NavLink></li>
+                    </ul>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <NavLink className="nav-link dropdown-toggle" to="games/sort-by" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Sort-by
+                    </NavLink>
+                    <ul className="dropdown-menu">
+                      <li><NavLink className="dropdown-item" to="games/sort-by/release-date">Release-Date</NavLink></li>
+                      <li><NavLink className="dropdown-item" to="games/sort-by/popularity">Popularity</NavLink></li>
+                      <li><NavLink className="dropdown-item" to="games/sort-by/alphabetical">Alphabetical</NavLink></li>
+                      <li><NavLink className="dropdown-item" to="games/sort-by/relevance">Relevance</NavLink></li>
+                    </ul>
+                  </li>
+                  <li className="nav-item dropdown">
+                    <NavLink className="nav-link dropdown-toggle" to="games/Categories" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                      Categories
+                    </NavLink>
+                    <ul className="dropdown-menu">
+                      <li><NavLink className="dropdown-item" to="games/Categories/racing">Racing</NavLink></li>
+                      <li><NavLink className="dropdown-item" to="games/Categories/sports">Sports</NavLink></li>
+                      <li><NavLink className="dropdown-item" to="games/Categories/socail">Social</NavLink></li>
+                      <li><NavLink className="dropdown-item" to="games/Categories/shooter">Shooter</NavLink></li>
+                      <li><NavLink className="dropdown-item" to="games/Categories/open-world">Open-World</NavLink></li>
+                      <li><NavLink className="dropdown-item" to="games/Categories/zombie">Zombie</NavLink></li>
+                      <li><NavLink className="dropdown-item" to="games/Categories/fantasy">Fantasy</NavLink></li>
+                      <li><NavLink className="dropdown-item" to="games/Categories/action-rpg">Action-rpg</NavLink></li>
+                      <li><NavLink className="dropdown-item" to="games/Categories/action">Action</NavLink></li>
+                      <li><NavLink className="dropdown-item" to="games/Categories/flight">Flight</NavLink></li>
+                      <li><NavLink className="dropdown-item" to="games/Categories/battle-royale">Battle-royale</NavLink></li>
+
                     </ul>
                   </li>
             </ul>
+            <div>
+              <NavLink className='text-decoration-none text-secondary me-3 fs-6' to='login'>Login</NavLink>
+              <NavLink className='btn btn-outline-info' to='register'>Join Free</NavLink>
+              <button className='btn btn-outline-info'>Logout</button>
+            </div>
         </div>
       </div>
 </nav>
