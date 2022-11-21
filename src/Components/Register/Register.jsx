b6d1da002b8567c5e2218fae071fcb947d1964b3
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-21 12:10:30 +0200

    add axios package to send register post request to server

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import axios from 'axios'
 import auth from '../../images/auth.jpg'
 import { Link, useNavigate } from 'react-router-dom'
 import Validation from '../../js/Validation'
