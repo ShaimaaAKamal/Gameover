c88cd0de87d8e38b2108752ca2f2efcef6b9e2fe
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-21 15:49:11 +0200

    add setToken

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -3,10 +3,10 @@ import { Outlet } from 'react-router-dom';
 import Navbar from '../Navbar/Navbar';
 
 
-export default function Layout({userData,setUser}) {
+export default function Layout({userData,setUser,setToken}) {
   return (
     <> 
-        <Navbar userData={userData} setUser={setUser}/>
+        <Navbar userData={userData} setUser={setUser} setToken={setToken}/>
         <Outlet/>
     </>
    )
