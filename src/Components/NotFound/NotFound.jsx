6c1007bc555cec1a602a54ce08c91b566693fe26
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-21 20:46:50 +0200

    add 404 image in the images folder and implement notfound page Component

--- src/Components/NotFound/NotFound.jsx
+++ src/Components/NotFound/NotFound.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import NotFoundImage from '../../images/404.png'
 
 export default function NotFound() {
   return (
-    <div>NotFound</div>
+    <div className='vh-100 d-flex align-items-center justify-content-center'>
+      <img src={NotFoundImage} alt="404 Image" className='w-100' />
+    </div>
   )
 }
