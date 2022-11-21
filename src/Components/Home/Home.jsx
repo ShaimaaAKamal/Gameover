07bef3139beafa92de162a58eaff182f1b60de6d
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-21 21:12:37 +0200

    implement header in home component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+        <div className='homeHeader text-center py-5'>
+           <h1 >Find & track the best <span className='text-info'>free-to-play </span>games!</h1>
+           <p className='text-muted'>Track what you've played and search for what to play next! Plus get free premium loot!</p>
+           <Link className='btn btn-outline-secondary mb-4' to='/games/all'>Browse Games</Link>
+        </div>
+    </>
   )
 }
