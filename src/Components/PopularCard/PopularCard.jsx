661c8f230e2879524ed1427618dd9822787a987c
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-22 22:53:58 +0200

    add componet implementation

--- src/Components/PopularCard/PopularCard.jsx
+++ src/Components/PopularCard/PopularCard.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
-export default function PopularCard() {
+export default function PopularCard({game,gameDetails}) {
   return (
-    <div>PopularCard</div>
+    <div className='HomeCard' onClick={()=>gameDetails(game.id)}>
+    <img src={game.thumbnail} alt={game.title} className='w-100' />
+    <div className='d-flex align-games-center justify-content-between p-3'>
+     <h5 className='text-capitalize'>{game.title}</h5>
+     <h5><span className="badge bg-info">Free</span></h5>
+    </div>
+ </div>
   )
 }
