37bcca10baf80042301da56356b1bf755aaba12b
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-22 20:20:53 +0200

    update all componet get data functions to be able to to get data for all routes

--- src/Components/All/All.jsx
+++ src/Components/All/All.jsx
@@ -1,44 +1,65 @@
 import React,{ useEffect, useRef, useState} from 'react';
-import { useLoaderData,useNavigate } from 'react-router-dom';
+import {useLocation, useNavigate, useParams } from 'react-router-dom';
 import DisplayedGames from '../DisplayGames/DisplayedGames';
-// import Loading from '../Loading/Loading'
-
-
+import Loading from '../Loading/Loading'
+import General from '../../js/General';
 
 
   
 export default function All() {
-  let index=useRef(24)
-  // let [load,setLoad]=useState(true)
-  const [more,setMore]=useState(true)
+  const params=useParams();
+  const location=useLocation()
+  console.log(location.pathname);
+  const general=new General();
+  let map=new Map(Object.entries(params))
+  let index=useRef(24);
+  let [load,setLoad]=useState(true)
+  let [more,setMore]=useState(true)
   const navigate=useNavigate()
-  const [games,setGames]=useState([])
-  const {data}=useLoaderData();
+  let [games,setGames]=useState([])
+  let [allData,setAllData]=useState([])
+  const fetchData=async()=>{
+    const {data}=await getApi()
+     if(data) {
+      setLoad(false);
+      setAllData(data);
+      if(index.current >=data.length) setMore(false)
+      setGames(data.slice(0,index.current))
+    }
+     }
   useEffect(()=>{
-    if(index.current >=data.length) setMore(false)
-    setGames(data.slice(0,index.current))
-    // setLoad(false)
+       fetchData()
   },[])
   useEffect(()=>{
-    if(index.current >= data.length) setMore(false)
-    setGames(data.slice(0,index.current))
-  },[data])
+    fetchData()
+  },[params])
   const gameDetails=(gameId)=>{
     navigate(`/gameDetails/${gameId}`)
   }
   const moreGames=()=>{ 
     index.current=index.current+24;
-    if( index.current <= data.length)
-     setGames(data.slice(0,index.current))
+    if( index.current <= allData.length)
+     setGames(allData.slice(0,index.current))
     else
-    { setGames(data.slice(0,data.length));
+    { setGames(allData.slice(0,allData.length));
       setMore(false);
     }
   }
 
+
+  const getApi=()=>{
+    if(map.size === 0 && location.pathname === '/games/all') return  general.getData('id','all','all');
+    else if(map.size === 0 && location.pathname === '/games/sort-by') return  general.getData('sort-by','');
+    else if(map.has('platform')) return  general.getData('platform',params.platform)
+    else if(map.has('sortItem')) return general.getData('sort-by',params.sortItem)
+    else if(map.has('category')) return  general.getData('category',params.category)
+  }
+
   return (
          <>
-         <DisplayedGames games={games} gameDetails={gameDetails} moreGames={moreGames} more={more}/>
+          {
+            (load)?<Loading/>:<DisplayedGames games={games} gameDetails={gameDetails} moreGames={moreGames} more={more}/>
+          }
           </>
   )
 }
