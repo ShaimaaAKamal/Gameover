4fbfeec337d031a536a0f2aab004ebad5465cbdd
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-22 15:32:58 +0200

    finish loading Component Design

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 export default function Loading() {
   return (
-     <div className='vh-100 d-flex justify-content-center align-items-cenetr'>
+     <div className='vh-75 d-flex justify-content-center align-items-center'>
          <div>
-
+              <i className="fa-regular fa-circle fa-2x mx-2 text-pink loadFarIcon"></i>
+              <i className="fa-solid fa-play fa-2x mx-2 fa-rotate-270 loadCloseIcon text-purple"></i>
+              <i className="fa-regular fa-square fa-2x mx-2 text-warning loadFarIcon"></i>
+              <i className="fa-solid fa-square fa-2x mx-2 text-info loadCloseIcon fa-rotate-45"></i>
          </div>
      </div>
   )
