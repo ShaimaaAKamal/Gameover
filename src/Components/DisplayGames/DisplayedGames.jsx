c738bd73447bf4d3875fd3247bb945d8205155b9
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-22 12:17:15 +0200

    pass more props to control displaying of more games btn

--- src/Components/DisplayGames/DisplayedGames.jsx
+++ src/Components/DisplayGames/DisplayedGames.jsx
@@ -3,17 +3,24 @@ import GameCard from '../GameCard/GameCard'
 
 
 
-export default function DisplayedGames({games,gameDetails}) {
+export default function DisplayedGames({games,gameDetails,moreGames,more}) {
   return (
     <div>
        <div className='container py-5 my-5'>
        <div className="row gy-4">
-       {
+          {
             games.map((game,index)=>  <div className="col-md-6 col-lg-3" key={index} >
                                           <GameCard game={game} gameDetails={gameDetails}/>
                                       </div>)
            }
        </div>
+       {
+       more && <div className='d-flex justify-content-center pt-5'>
+        <button className='btn btn-outline-secondary d-flex align-items-center' onClick={moreGames}>
+            <span>More Games</span>
+            <i className="ms-1 fa-solid fa-chevron-right"></i>
+       </button>
+       </div>}
     </div>
     </div>
   )
