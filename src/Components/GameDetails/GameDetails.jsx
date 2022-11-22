bc7b4ad0e8320924cdc0322ae742ea885a78285b
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-22 20:22:52 +0200

    update game details page to get data from component did mount intead of loader

--- src/Components/GameDetails/GameDetails.jsx
+++ src/Components/GameDetails/GameDetails.jsx
@@ -1,111 +1,132 @@
-import React, { useState } from 'react'
-import { useLoaderData } from 'react-router-dom';
+import React, { useState ,useEffect } from 'react'
 import Carousel from 'react-bootstrap/Carousel';
+import { useParams } from 'react-router-dom';
+import General from '../../js/General';
+import Loading from '../Loading/Loading';
 export default function GameDetails() {
-  const {data}=useLoaderData();
-  let reqKeys,reqValues
-  const [game,setGame]=useState(data);
+  const params=useParams();
+  const general=new General();
+  let [reqKeys,setReqKeys]=useState([])
+  let [reqValues,setReqValues]=useState([])
+  const [game,setGame]=useState({});
+  const [load,setLoad]=useState(true)
   const [index, setIndex] = useState(0);
-  if(game['minimum_system_requirements']){
-    reqKeys=Object.keys(game['minimum_system_requirements']);
-    reqValues=Object.values(game['minimum_system_requirements']);
-  }
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
 
+  useEffect(()=>{
+    async function fetchData(){
+     const {data}=await general.getData('id',params.id,'game')
+      if(data) {
+       setLoad(false);
+       setGame(data);
+       if(data['minimum_system_requirements']){
+        setReqKeys(Object.keys(data['minimum_system_requirements']));
+        setReqValues(Object.values(data['minimum_system_requirements']));
+      }
+     }
+      }
+      fetchData()
+ },[])
   return (
    ( <div className='container py-5 my-5'>
-      <div className="row">
-         <div className="col-lg-4">
-              <div>
-                 <img src={game.thumbnail} alt={game.title} className='w-100 rounded' />
-                 <div className='py-2 d-flex'>
-                    <span className="badge bg-secondary detailsPage w-25 me-2">Free</span>
-                    <div className='w-75'>
-                         <a className='btn btn-info d-flex w-100 justify-content-center align-items-center text-white' href={game['game_url']}>
-                          <span>Play Now</span>
-                          <i className="fa-solid ms-1 fa-right-from-bracket"></i>
-                          </a>
-                    </div>
+       {
+        (load)?<Loading/>:<div className="row">
+        <div className="col-lg-4">
+             <div>
+                <img src={game.thumbnail} alt={game.title} className='w-100 rounded' />
+                <div className='py-2 d-flex'>
+                   <span className="badge bg-secondary detailsPage w-25 me-2">Free</span>
+                   <div className='w-75'>
+                        <a className='btn btn-info d-flex w-100 justify-content-center align-items-center text-white' href={game['game_url']}>
+                         <span>Play Now</span>
+                         <i className="fa-solid ms-1 fa-right-from-bracket"></i>
+                         </a>
+                   </div>
+                </div>
+             </div>
+        </div>
+        <div className="col-lg-8">
+             <div className='text-white-50'>
+                <h2 className='pb-2 fs-1'>{game.title}</h2>
+                <h4>About {game.title}</h4>
+                <p>{game.description}</p>
+                 {game['minimum_system_requirements']&& (
+                       <>
+                           <h4 className='py-3'>Minimum System Requirements</h4>
+                           <div>
+                             {
+                               reqValues.map((reqValue,index)=> ( <div className='d-flex align-items-center' key={index}>
+                               <h6 className='my-0 fw-bolder'>{reqKeys[index]}: </h6>
+                               <p className='my-0 fs-6'>{reqValue}</p>
+                             </div>))
+                             }
+                           </div>
+                       </>
+                    )
+                 }
+                 {
+                  (game.screenshots.length !==0) ? 
+                  <>  
+                  <h4 className='py-4'>game name Screenshots</h4>
+                  <Carousel activeIndex={index} onSelect={handleSelect} controls={false} indicators={false} interval={3000}>
+                     {game.screenshots.map((slide, i) => {
+                       return (
+                         <Carousel.Item key={i}>        
+                       <img
+                         className="d-block w-100"
+                         src={slide.image}
+                         alt="slider image"
+                       />
+                     </Carousel.Item>)})}
+                   </Carousel></>:''
+                 }
+                 <h4 className='py-4'>Additional Information</h4>
+                 <div className="row gy-4">
+                   <div className="col-lg-4 col-6">
+                        <div>
+                           <p className='my-0 mb-2 text-muted'>Title</p>
+                           <p className='my-0 text-capitalize'>{game.title}</p>
+                        </div>
+                   </div>
+                   <div className="col-lg-4 col-6">
+                        <div>
+                           <p className='my-0 mb-2 text-muted'>Developer</p>
+                           <p className='my-0 text-capitalize'>{game.developer}</p>
+                        </div>
+                   </div>
+                   <div className="col-lg-4 col-6">
+                        <div>
+                           <p className='my-0 mb-2 text-muted'>Publisher</p>
+                           <p className='my-0 text-capitalize'>{game.publisher}</p>
+                        </div>
+                   </div>
+                   <div className="col-lg-4 col-6">
+                        <div>
+                           <p className='my-0 mb-2 text-muted'>Release Date</p>
+                           <p className='my-0 text-capitalize'>{game['release_date']}</p>
+                        </div>
+                   </div>
+                   <div className="col-lg-4 col-6">
+                        <div>
+                           <p className='my-0 mb-2 text-muted'>Genre</p>
+                           <p className='my-0 text-capitalize'>{game.genre}</p>
+                        </div>
+                   </div>
+                   <div className="col-lg-4 col-6">
+                        <div>
+                           <p className='my-0 mb-2 text-muted'>Platform</p>
+                           <p className='my-0 text-capitalize'>
+                             <span>{(game.platform === 'Web Browser')?(<><i className="fa-solid fa-window-maximize me-2"></i><span>Browser</span></>):<><i className="fa-brands me-2 fa-windows"></i><span>Windows</span></>}</span>
+                           </p>
+                        </div>
+                   </div>
                  </div>
-              </div>
-         </div>
-         <div className="col-lg-8">
-              <div className='text-white-50'>
-                 <h2 className='pb-2 fs-1'>{game.title}</h2>
-                 <h4>About {game.title}</h4>
-                 <p>{game.description}</p>
-                  {game['minimum_system_requirements']&& (
-                        <>
-                            <h4 className='py-3'>Minimum System Requirements</h4>
-                            <div>
-                              {
-                                reqValues.map((reqValue,index)=> ( <div className='d-flex align-items-center' key={index}>
-                                <h6 className='my-0 fw-bolder'>{reqKeys[index]}: </h6>
-                                <p className='my-0 fs-6'>{reqValue}</p>
-                              </div>))
-                              }
-                            </div>
-                        </>
-                     )
-                  }
-                 <h4 className='py-4'>game name Screenshots</h4>
-                 <Carousel activeIndex={index} onSelect={handleSelect} controls={false} indicators={false} interval={3000}>
-                    {game.screenshots.map((slide, i) => {
-                      return (
-                        <Carousel.Item key={i}>        
-                      <img
-                        className="d-block w-100"
-                        src={slide.image}
-                        alt="slider image"
-                      />
-                    </Carousel.Item>)})}
-                  </Carousel>
-                  <h4 className='py-4'>Additional Information</h4>
-                  <div className="row gy-4">
-                    <div className="col-lg-4 col-6">
-                         <div>
-                            <p className='my-0 mb-2 text-muted'>Title</p>
-                            <p className='my-0 text-capitalize'>{game.title}</p>
-                         </div>
-                    </div>
-                    <div className="col-lg-4 col-6">
-                         <div>
-                            <p className='my-0 mb-2 text-muted'>Developer</p>
-                            <p className='my-0 text-capitalize'>{game.developer}</p>
-                         </div>
-                    </div>
-                    <div className="col-lg-4 col-6">
-                         <div>
-                            <p className='my-0 mb-2 text-muted'>Publisher</p>
-                            <p className='my-0 text-capitalize'>{game.publisher}</p>
-                         </div>
-                    </div>
-                    <div className="col-lg-4 col-6">
-                         <div>
-                            <p className='my-0 mb-2 text-muted'>Release Date</p>
-                            <p className='my-0 text-capitalize'>{game['release_date']}</p>
-                         </div>
-                    </div>
-                    <div className="col-lg-4 col-6">
-                         <div>
-                            <p className='my-0 mb-2 text-muted'>Genre</p>
-                            <p className='my-0 text-capitalize'>{game.genre}</p>
-                         </div>
-                    </div>
-                    <div className="col-lg-4 col-6">
-                         <div>
-                            <p className='my-0 mb-2 text-muted'>Platform</p>
-                            <p className='my-0 text-capitalize'>
-                              <span>{(game.platform === 'Web Browser')?(<><i className="fa-solid fa-window-maximize me-2"></i><span>Browser</span></>):<><i className="fa-brands me-2 fa-windows"></i><span>Windows</span></>}</span>
-                            </p>
-                         </div>
-                    </div>
-                  </div>
-              </div>
-         </div>
-      </div>
+             </div>
+        </div>
+     </div>
+       }
     </div>)
   )
 }
