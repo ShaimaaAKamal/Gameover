0b0e461d4ee7f82185910e8e885158bc81da248d
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-22 11:42:01 +0200

    implement gameCard component

--- src/Components/GameCard/GameCard.jsx
+++ src/Components/GameCard/GameCard.jsx
@@ -1,7 +1,26 @@
 import React from 'react'
 
-export default function GameCard() {
+export default function GameCard({game,gameDetails}) {
   return (
-    <div>GameCard</div>
+            <div className='HomeCard' onClick={()=>gameDetails(game.id)}>
+               <img src={game.thumbnail} alt={game.title} className='w-100' />
+               <div className='p-3'>
+                  <div className='d-flex align-games-center justify-content-between'>
+                    <h5 className='text-capitalize'>{((game['title']).length >12)?
+                    `${game['title'].split('').slice(0,12).join('')}....`:game['title']}</h5>
+                    <h5><span className="badge bg-info">Free</span></h5>
+                  </div>
+                  <p className='text-muted small my-0'>
+                    {`${game['short_description'].split('').slice(0,25).join('')}....`}
+                  </p>
+                  <div className='d-flex align-games-center justify-content-between pt-3'>
+                      <i className="fa-solid fa-square-plus"></i>
+                      <div className=' small d-flex align-games-center'>
+                           <span className='me-2 category'>{game.genre}</span>
+                           <span className='platformIcon'>{(game.platform === 'Web Browser')?<i className="fa-solid fa-window-maximize"></i>:<i className="fa-brands fa-windows"></i>}</span>
+                      </div>
+                  </div>
+               </div>
+            </div>
   )
 }
