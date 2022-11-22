96668356da3268e8d3ca7c5e6112441c5c933baf
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-22 11:42:34 +0200

    implement displaydGames  component

--- src/Components/DisplayGames/DisplayedGames.jsx
+++ src/Components/DisplayGames/DisplayedGames.jsx
@@ -1,12 +1,20 @@
 import React, { useState } from 'react'
-import { useLoaderData, useLocation } from 'react-router-dom'
+import GameCard from '../GameCard/GameCard'
 
 
 
-export default function DisplayedGames() {
-  const response =useLoaderData();
-  const [games,setGames]=useState(response.data) 
+export default function DisplayedGames({games,gameDetails}) {
   return (
-    <div>DisplayedGames</div>
+    <div>
+       <div className='container py-5 my-5'>
+       <div className="row gy-4">
+       {
+            games.map((game,index)=>  <div className="col-md-6 col-lg-3" key={index} >
+                                          <GameCard game={game} gameDetails={gameDetails}/>
+                                      </div>)
+           }
+       </div>
+    </div>
+    </div>
   )
 }
