3ca4d5d52f47911b42d9ea76d5dbd10d6d98e9fe
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-22 12:17:55 +0200

    add more games feature to the component

--- src/Components/All/All.jsx
+++ src/Components/All/All.jsx
@@ -1,4 +1,4 @@
-import React,{ useEffect, useState} from 'react';
+import React,{ useEffect, useRef, useState} from 'react';
 import { useLoaderData,useNavigate } from 'react-router-dom';
 import DisplayedGames from '../DisplayGames/DisplayedGames';
 
@@ -6,16 +6,29 @@ import DisplayedGames from '../DisplayGames/DisplayedGames';
 
   
 export default function All() {
+  let index=useRef(24)
+  const [more,setMore]=useState(true)
   const navigate=useNavigate()
   const [games,setGames]=useState([])
   const {data}=useLoaderData();
   useEffect(()=>{
-    setGames(data.slice(0,19))
+    setGames(data.slice(0,index.current))
   },[])
   const gameDetails=(gameId)=>{
     navigate(`/gameDetails/${gameId}`)
   }
+  const moreGames=()=>{ 
+    index.current=index.current+24;
+    if( index.current <= data.length)
+     setGames(data.slice(0,index.current))
+    else
+    { setGames(data.slice(0,data.length));
+      setMore(false);
+    }
+  }
   return (
-         <DisplayedGames games={games} gameDetails={gameDetails}/>
+         <>
+            <DisplayedGames games={games} gameDetails={gameDetails} moreGames={moreGames} more={more}/>
+         </>
   )
 }
