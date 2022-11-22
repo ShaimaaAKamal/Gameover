15e6a101c30c47ffc7a6f8ac444c77363eb3d9f2
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-22 11:43:11 +0200

    reimplement all component using displayedGame component

--- src/Components/All/All.jsx
+++ src/Components/All/All.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React,{ useEffect, useState} from 'react';
+import { useLoaderData,useNavigate } from 'react-router-dom';
+import DisplayedGames from '../DisplayGames/DisplayedGames';
 
+
+
+  
 export default function All() {
+  const navigate=useNavigate()
+  const [games,setGames]=useState([])
+  const {data}=useLoaderData();
+  useEffect(()=>{
+    setGames(data.slice(0,19))
+  },[])
+  const gameDetails=(gameId)=>{
+    navigate(`/gameDetails/${gameId}`)
+  }
   return (
-    <div>All</div>
+         <DisplayedGames games={games} gameDetails={gameDetails}/>
   )
 }
