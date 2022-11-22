44e558151c3be41c5f7c7e6cd15d1add6c830b78
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-22 14:46:03 +0200

    start loading component implemnation

--- src/Components/Loading/Loading.jsx
+++ src/Components/Loading/Loading.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Loading() {
   return (
-    <div>Loading</div>
+     <div className='vh-100 d-flex justify-content-center align-items-cenetr'>
+         <div>
+
+         </div>
+     </div>
   )
 }
