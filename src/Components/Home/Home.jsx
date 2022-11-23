72b28fbae6abd0efb00977fbd80177387f85a882
Shaimaa Kamal <101939912+ShaimaaAKamal@users.noreply.github.com>
2022-11-23 11:07:44 +0200

    adjsu layout spacing

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -25,7 +25,7 @@ export default function Home() {
     <div className='container'>
       {(load)?<Loading/>:<>
       <div className='homeHeader text-center py-5'>
-           <h1 >Find & track the best <span className='text-info'>free-to-play </span>games!</h1>
+           <h1 className='pt-5' >Find & track the best <span className='text-info'>free-to-play </span>games!</h1>
            <p className='text-muted'>Track what you've played and search for what to play next! Plus get free premium loot!</p>
            <Link className='btn btn-outline-secondary mb-4' to='/games/all'>Browse Games</Link>
         </div>
